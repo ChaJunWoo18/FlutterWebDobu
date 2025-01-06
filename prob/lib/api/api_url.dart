f9054955b2e10feca90e270dfb424d230b644551
ChaJunWoo18 <105590071+ChaJunWoo18@users.noreply.github.com>
2025-01-06 15:55:24 +0900

    Update api_url.dart

--- prob/lib/api/api_url.dart
+++ prob/lib/api/api_url.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = "http://dobu-fastapi:8000";
+  static const String baseUrl = "http://54.153.14.174:8000";
 }
 
 //   static const String baseUrl = "http://54.153.14.174:8000";
