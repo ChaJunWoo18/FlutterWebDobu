18777f813970d7c6dacf6cc8b4278fbb9bc642dd
ChaJunWoo18 <105590071+ChaJunWoo18@users.noreply.github.com>
2025-01-08 22:37:01 +0900

    Update api_url.dart

--- prob/lib/api/api_url.dart
+++ prob/lib/api/api_url.dart
@@ -1,5 +1,3 @@
 class ApiConstants {
-  static const String baseUrl = "http://54.153.14.174:8000/api";
+  static const String baseUrl = "http://54.153.14.174:8000";
 }
-
-//   static const String baseUrl = "http://54.153.14.174:8000";
